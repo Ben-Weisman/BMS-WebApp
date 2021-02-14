e5a2fb0957cf388d868f5ff89562a5c4c8318fb1
Ben-Weisman <54437197+Ben-Weisman@users.noreply.github.com>
2021-02-14 16:38:29 +0200

    Worked on UpdateMemberDetailsServlet

--- BMSWebApp/src/constants/Constants.java
+++ BMSWebApp/src/constants/Constants.java
@@ -10,5 +10,11 @@ public class Constants {
     public static final String MAIN_MENU_URL_REGULAR = "mainMenuRegular.html";
     public static final String LOGIN_PAGE_URL = "index.html";
     public static final String MEMBER_FIELD_TO_EDIT_PARAMETER_NAME = "fieldToUpdate";
+    public static final String MEMBER_ID = "memberID";
+
+    public static final String MEMBER_NAME_FIELD_NAME = "name";
+    public static final String MEMBER_PHONE_FIELD_NAME = "phone";
+    public static final String MEMBER_EMAIL_FIELD_NAME = "email";
+    public static final String MEMBER_PASSWORD_FIELD_NAME = "password";
 
 }
