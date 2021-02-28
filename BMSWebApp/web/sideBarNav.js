56d0946f2a852240969ae531d6a4587605f18937
Ben-Weisman <54437197+Ben-Weisman@users.noreply.github.com>
2021-02-28 14:53:03 +0200

    Worked on maintain feature

--- BMSWebApp/web/sideBarNav.js
+++ BMSWebApp/web/sideBarNav.js
@@ -3,17 +3,17 @@ const navBarEL = document.getElementById("sideNav");
 function injectAdminSideNavBar(){
     navBarEL.innerHTML = "<nav>\n" +
         "    <ul class=\"nav\">\n" +
-        "        <li><a href=\"updatePersonalInfo.html\">Update Personal Info</a> </li>\n" +
-        "        <li><a href=\"submitNewBookingRequest.html\">Submit New Booking Request</a> </li>\n" +
-        "        <li><a href=\"showAllFutureBookingRequests.html\">Show All Future Booking Requests</a> </li>\n" +
-        "        <li><a href=\"showRequestsHistory.html\">Show Requests History</a> </li>\n" +
-        "        <li><a href=\"maintain.html\">Maintain</a> </li>\n" +
-        "        <li><a href=\"watchBookingRequests.html\">Watch Booking Requests</a> </li>\n" +
-        "        <li><a href=\"importFromXML.html\">Import From XML</a> </li>\n" +
-        "        <li><a href=\"exportToXML.html\">Export To XML</a> </li>\n" +
-        "        <li><a href=\"addNotification.html\">Add Notification</a> </li>\n" +
-        "        <li><a href=\"deleteNotification.html\">Delete Notification</a> </li>\n" +
-        "        <li><a href=\"logout\">Logout</a> </li>\n " +
+        "        <li><a href=\"/BMSWebApp/updatePersonalInfo.html\">Update Personal Info</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/submitNewBookingRequest.html\">Submit New Booking Request</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/showAllFutureBookingRequests.html\">Show All Future Booking Requests</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/showRequestsHistory.html\">Show Requests History</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/maintain.html\">Maintain</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/watchBookingRequests.html\">Watch Booking Requests</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/importFromXML.html\">Import From XML</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/exportToXML.html\">Export To XML</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/addNotification.html\">Add Notification</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/deleteNotification.html\">Delete Notification</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/logout\">Logout</a> </li>\n " +
         "    </ul>\n" +
         "</nav>";
 
@@ -21,17 +21,17 @@ function injectAdminSideNavBar(){
 function injectRegularSideBar(){
     navBarEL.innerHTML = "<nav>\n" +
         "    <ul class=\"nav\">\n" +
-        "        <li><a href=\"updatePersonalInfo.html\">Update Personal Info</a> </li>\n" +
-        "        <li><a href=\"submitNewBookingRequest.html\">Submit New Booking Request</a> </li>\n" +
-        "        <li><a href=\"showAllFutureBookingRequests.html\">Show All Future Booking Requests</a> </li>\n" +
-        "        <li><a href=\"showRequestsHistory.html\">Show Requests History</a> </li>\n" +
-        "        <li><a href=\"logout\">Logout</a> </li>\n " +
+        "        <li><a href=\"/BMSWebApp/updatePersonalInfo.html\">Update Personal Info</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/submitNewBookingRequest.html\">Submit New Booking Request</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/showAllFutureBookingRequests.html\">Show All Future Booking Requests</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/showRequestsHistory.html\">Show Requests History</a> </li>\n" +
+        "        <li><a href=\"/BMSWebApp/logout\">Logout</a> </li>\n " +
         "    </ul>\n" +
         "</nav>";
 }
 
 async function isAdmin() {
-    let response = await fetch('adminVerify');
+    let response = await fetch('/BMSWebApp/adminVerify');
     let json =  await response.json();
     console.log(json.isAdmin)
     return json.isAdmin;
